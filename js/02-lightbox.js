137b64a2a5cbde4c49ad5402626f2d50be8291e4
Hanna Pokamiestova <105422641+HannaPokamiestova@users.noreply.github.com>
2023-04-30 20:11:23 +0200

    Update #2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+function getGalleryItemHtml(sourse, src, alt) {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href="${sourse}">
+    <img
+      class="gallery__image"
+      src="${src}"
+      data-source="${sourse}"
+      alt="${alt}"
+    />
+  </a>
+</li>`;
+}
+
+const galleryItemsHTML = galleryItems
+  .map((item) => {
+    return getGalleryItemHtml(item.original, item.preview, item.description);
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryItemsHTML);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
